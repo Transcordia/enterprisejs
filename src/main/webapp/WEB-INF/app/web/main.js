2ae45f5c5b205361cf2845498696e35bbba90dbb
jameshines10 <jhines@pykl.com>
2012-06-11 16:38:08 -0400

    Added backbone boilerplate for proof of concept

--- src/main/webapp/WEB-INF/app/web/main.js
+++ src/main/webapp/WEB-INF/app/web/main.js
@@ -3,13 +3,16 @@
  */
 var log = require('ringo/logging').getLogger(module.id);
 var {json} = require('ringo/jsgi/response');
+var {trimpathResponse} = require('trimpath');
 
 var {Application} = require("stick");
 var app = exports.app = Application();
 app.configure('notfound', 'params', 'route');
 
 app.get('/', function (req) {
-    return json({web:true});
+    //return json({web:true});
+
+    return trimpathResponse(module.resolve('views/index.html'));
 });
 
 
