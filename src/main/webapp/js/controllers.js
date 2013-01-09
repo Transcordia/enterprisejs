8517d0178103db7d9ee54d675378bbe44feb027a
jameshines10 <jhines@pykl.com>
2013-01-09 15:05:48 -0500

    Isotope is integrated with the ejs project 	modified:   src/main/webapp/css/app.css 	modified:   src/main/webapp/index.html 	modified:   src/main/webapp/js/controllers.js 	modified:   src/main/webapp/js/directives.js 	modified:   src/main/webapp/lib/isotope/css/style.css 	modified:   src/main/webapp/partials/home.html

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -6,7 +6,7 @@
 function HomeCtrl($scope) {
     $scope.articles = [
         {
-            description: 'Bicycle rights keffiyeh church-key farm-to-table, wolf freegan meggings food truck +1 helvetica craft beer hella.',
+            description: 'Bicycle rights keffiyeh church-key farm-to-table, wolf freegan meggings food truck +1 helvetica craft beer hella. Bicycle rights keffiyeh church-key farm-to-table, wolf freegan meggings food truck +1 helvetica craft beer hella.',
             stats: {
                 likes: 29,
                 comments: 30,
@@ -23,7 +23,7 @@ function HomeCtrl($scope) {
 
         },
         {
-            description: 'Bicycle rights keffiyeh church-key farm-to-table, wolf freegan meggings food truck +1 helvetica craft beer hella.',
+            description: 'Portland stumptown ethical freegan, VHS church-key selvage ugh.',
             stats: {
                 likes: 29,
                 comments: 30,
@@ -40,7 +40,24 @@ function HomeCtrl($scope) {
 
         },
         {
-            description: 'Bicycle rights keffiyeh church-key farm-to-table, wolf freegan meggings food truck +1 helvetica craft beer hella.',
+            description: 'Salvia food truck trust fund, synth twee american apparel photo booth freegan squid marfa aesthetic hashtag craft beer mlkshk.',
+            stats: {
+                likes: 29,
+                comments: 30,
+                reblogs: 5
+            },
+            convoStarter: {
+                content: 'Raw denim +1 truffaut keytar, squid polaroid next level mixtape american apparel pug try-hard retro wes anderson viral. Forage next level keytar echo park actually fingerstache banh mi cray plaid, squid locavore sartorial. Chillwave art party mustache sustainable, swag put a bird on it american apparel cosby sweater. Tattooed mustache chambray messenger bag, odd future typewriter banh mi portland whatever meggings raw denim hella ethical fingerstache.'
+            },
+            comments: [
+                {
+                    content: 'This is a comment'
+                }
+            ]
+
+        },
+        {
+            description: 'Letterpress art party put a bird on it, pop-up typewriter authentic swag polaroid flannel whatever salvia biodiesel.',
             stats: {
                 likes: 29,
                 comments: 30,
@@ -57,7 +74,7 @@ function HomeCtrl($scope) {
 
         },
         {
-            description: 'Bicycle rights keffiyeh church-key farm-to-table, wolf freegan meggings food truck +1 helvetica craft beer hella.',
+            description: 'Austin intelligentsia bespoke, williamsburg disrupt beard VHS before they sold out farm-to-table. +1 echo park fashion axe, four loko plaid tattooed skateboard. Vice odd future post-ironic brunch sustainable pinterest.',
             stats: {
                 likes: 29,
                 comments: 30,
@@ -74,7 +91,24 @@ function HomeCtrl($scope) {
 
         },
         {
-            description: 'Bicycle rights keffiyeh church-key farm-to-table, wolf freegan meggings food truck +1 helvetica craft beer hella.',
+            description: "Fingerstache next level try-hard american apparel, skateboard ennui etsy banksy meggings locavore kale chips. High life typewriter organic polaroid try-hard mustache plaid, mumblecore pop-up fingerstache pork belly. Vinyl swag pug aesthetic, pork belly cred beard kale chips four loko quinoa godard next level flannel marfa. Occupy lomo mcsweeney's mumblecore, locavore intelligentsia pork belly brooklyn banksy.",
+            stats: {
+                likes: 29,
+                comments: 30,
+                reblogs: 5
+            },
+            convoStarter: {
+                content: 'Skateboard polaroid iphone butcher flexitarian aesthetic sriracha.'
+            },
+            comments: [
+                {
+                    content: 'This is a comment'
+                }
+            ]
+
+        },
+        {
+            description: 'Stumptown wes anderson literally banksy, bicycle rights food truck butcher craft beer ethical. Banksy bushwick 8-bit, DIY chillwave PBR typewriter. Carles tousled wayfarers, gentrify direct trade Austin small batch scenester irony yr vice. Ugh keytar viral, umami gluten-free banjo gentrify pop-up gastropub blue bottle direct trade. Tumblr bicycle rights cliche, terry richardson sartorial beard sustainable. Ethical viral chillwave sustainable wolf biodiesel.',
             stats: {
                 likes: 29,
                 comments: 30,
@@ -92,4 +126,4 @@ function HomeCtrl($scope) {
         }
     ]
 }
-HomeCtrl.$inject = [];
+HomeCtrl.$inject = ["$scope"];
--- src/main/webapp/js/directives.js
+++ src/main/webapp/js/directives.js
@@ -1,11 +1,59 @@
 'use strict';
 
 /* Directives */
-
+angular.module('ejs.directives', []);
 
 angular.module('ejs.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+    directive('isotope', function () {
+        return {
+            scope: {
+                articles: '='
+            },
+            link: function (scope, element, attrs) {
+                var options = {
+                    animationEngine : 'best-available',
+                    itemSelector: '.article',
+                    layoutMode: 'masonry'
+                };
+
+                var init = function() {
+                    element.isotope(options);
+                };
+
+                var setup = function () {
+                    var articles = '';
+                    scope.articles.forEach(function (article) {
+                        articles += '<div class="article">\
+                                        <div class="article-holder"></div>\
+                                        <p class="description">' + article.description + '</p>\
+                                        <p class="stats clearfix">\
+                                            <span class="likes-count">' + article.stats.likes + ' likes</span>\
+                                            <span class="comment-count">' + article.stats.comments + ' comments</span>\
+                                            <span class="reblog-count">' + article.stats.reblogs + ' reblogs</span>\
+                                        </p>\
+                                        <div class="convo clearfix">\
+                                            <a href=""><div class="image-placeholder"></div></a>\
+                                            <p>' + article.convoStarter.content + '</p>\
+                                        </div>\
+                                        <div class="comments">\
+                                            <div class="comment convo">\
+                                                <a href=""><div class="image-placeholder"></div></a>\
+                                                <p>' + article.comments[0].content + '</p>\
+                                            </div>\
+                                        </div>\
+                                    </div>'
+                    });
+
+                    element.isotope('remove', element.find('article'));
+                    element.isotope('insert', $(articles));
+                };
+
+                scope.$watch('articles', function (newValue, oldValue) {
+                    if (newValue.length == 0) init();
+                    if (newValue.length > 0) setup();
+                });
+
+                init();
+            }
+        };
+    });
